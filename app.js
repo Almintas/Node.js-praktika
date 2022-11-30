f5ce7febfcbc6ca808f0736281df9003b8113c2c
Almintas <almintasbruzas@gmail.com>
2022-11-30 21:25:18 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = 3000;
+
+app.use(cors());
+
+
+
+app.listen(PORT, () => console.log(`App is running on port  ${PORT}`));
